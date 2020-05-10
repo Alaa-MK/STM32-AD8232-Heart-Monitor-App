9bcd5e88c97fde38ec3cca14cb284d62e27b3289
Alaa-MK <alaa44010@aucegypt.edu>
2020-05-10 15:37:56 +0200

    fixed the slow graph updating

--- appWindow.py
+++ appWindow.py
@@ -10,22 +10,32 @@ from ui import Ui_Dialog
 class Graph():
     def __init__(self):
         self.startingTime = time.time()
-        self.MAX_SIZE = 40
         self.timeData = []
         self.data = []
         self.canvas = FigureCanvas(Figure(figsize=(6.4, 2.3)))
         self.ax = self.canvas.figure.subplots()
+        self.line, = self.ax.plot(self.timeData, self.data)
+        self.ax.set_ylim(0, 1)
+        self.updateRate = 10
+        self.lastUpdate = time.time()
+        self.timeRange = 5      #shows only the last 5 seconds
 
     def append(self, value):
-        self.ax.clear()
         self.data.append(value)
         self.timeData.append(time.time()-self.startingTime)
-        if len(self.data) > self.MAX_SIZE:
+        if time.time() - self.startingTime - self.timeData[0] > self.timeRange:
             self.data.pop(0)
             self.timeData.pop(0)
-        self.ax.set_ylim(0, 1)
-        self.ax.plot(self.timeData, self.data)
-        self.ax.figure.canvas.draw()
+        if (time.time() - self.lastUpdate > 1/self.updateRate):
+            self.updateGraph()
+            self.lastUpdate = time.time()
+    
+    def updateGraph(self):
+        self.line.set_xdata(self.timeData)
+        self.line.set_ydata(self.data)
+        self.ax.set_xlim(self.timeData[-1] - self.timeRange, self.timeData[-1] + self.timeRange)
+        self.canvas.figure.canvas.draw()
+
         
 
 class ApplicationWindow(QtWidgets.QMainWindow):
